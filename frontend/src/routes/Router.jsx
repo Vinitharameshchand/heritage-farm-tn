bcde3dc7c2369b2b1f0e5af8702c004bea2c5e76
neuervinch <npandian515@gmail.com>
2026-01-21 02:57:22 +0530

    update routing

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Discover from '../pages/Discover';
-// import ListingDetail from '../pages/ListingDetail';
+import ListingDetail from '../pages/ListingDetail';
 // import Login from '../pages/Login';
 // import Signup from '../pages/Signup';
 
@@ -11,7 +11,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/discover" element={<Discover />} />
-                {/* <Route path="/listings/:id" element={<ListingDetail />} /> */}
+                <Route path="/listings/:id" element={<ListingDetail />} />
                 {/* <Route path="/login" element={<Login />} /> */}
                 {/* <Route path="/signup" element={<Signup />} /> */}
             </Routes>
