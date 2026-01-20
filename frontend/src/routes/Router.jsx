2dabee0f47048a6c2722edaf2594b218aba3531d
neuervinch <npandian515@gmail.com>
2026-01-21 03:16:44 +0530

    bug fix

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import Home from '../pages/Home';
 import Discover from '../pages/Discover';
 import ListingDetail from '../pages/ListingDetail';
-// import Login from '../pages/Login';
-// import Signup from '../pages/Signup';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/discover" element={<Discover />} />
                 <Route path="/listings/:id" element={<ListingDetail />} />
-                {/* <Route path="/login" element={<Login />} /> */}
-                {/* <Route path="/signup" element={<Signup />} /> */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
             </Routes>
         </BrowserRouter>
     );
