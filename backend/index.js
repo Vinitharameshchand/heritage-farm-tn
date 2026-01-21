7a416f73f58bb65d3d8f268e7865eda9243a5096
Naveen pandian <npandian515@gmail.com>
2026-01-21 08:52:20 +0530

    cleanup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
 import { dirname, join } from 'path';
